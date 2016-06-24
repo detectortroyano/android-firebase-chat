977c7e6df8055b7296712c0ee51866eb15800f23
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2016-06-23 23:48:32 -0500

    AndriodChat Chat Login Butterknife

--- app/src/main/java/edu/detectortroyano/com/adroidchat/LoginActivity.java
+++ app/src/main/java/edu/detectortroyano/com/adroidchat/LoginActivity.java
@@ -2,12 +2,34 @@ package edu.detectortroyano.com.adroidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.editTxtEmail)
+    EditText editTxtEmail;
+    @Bind(R.id.editTxtPassword)
+    EditText editTxtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignin)
+    public void handleSignin(){
+        Log.e("Click",editTxtEmail.getText().toString());
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void handleSignup(){
+        Log.e("Click",editTxtPassword.getText().toString());
     }
 }

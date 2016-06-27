27f76d1bc8093ffaf303f07371411807f4944c7c
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2016-06-26 22:53:10 -0500

    AndriodChat Chat Login Presentador

---
+++ app/src/main/java/edu/detectortroyano/com/adroidchat/login/LoginPresenterImpl.java
@@ -0,0 +1,74 @@
+package edu.detectortroyano.com.adroidchat.login;
+
+/**
+ * Created by detectortroyano on 26/06/2016.
+ */
+public class LoginPresenterImpl implements LoginPresenter{
+
+    LoginView loginView;
+    LoginInteractor loginInteractor;
+
+    public LoginPresenterImpl(LoginView loginView) {
+        this.loginView = loginView;
+    }
+
+    @Override
+    public void onDestroy() {
+        loginView = null;
+    }
+
+    @Override
+    public void checkForAuthenticatedUser() {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+        loginInteractor.checkSession();
+    }
+
+    @Override
+    public void valideLogin(String email, String password) {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+        loginInteractor.doSignIn(email,password);
+    }
+
+    @Override
+    public void registerNewUser(String email, String password) {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+        loginInteractor.doSignUp(email,password);
+    }
+
+    private void onSignInSuccess(){
+        if(loginView != null){
+            loginView.navigateToMainScreen();
+        }
+    }
+
+    private void onSignUpSuccess(){
+        if(loginView != null){
+            loginView.newUserSucces();
+        }
+    }
+
+    private void onSignInError(String error){
+        if(loginView != null){
+            loginView.hideProgress();
+            loginView.enableInputs();
+            loginView.loginError(error);
+        }
+    }
+
+    private void onSignUpError(String error){
+        if(loginView != null){
+            loginView.hideProgress();
+            loginView.enableInputs();
+            loginView.newUserError(error);
+        }
+    }
+}
